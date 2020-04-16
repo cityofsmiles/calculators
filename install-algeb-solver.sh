8886696ab9f4e5f2a68dd32c4a3bcc47a2c46873
jon <cityofsmiles@gmail.com>
2020-04-16 10:21:08 +0800

    edited installer for algeb solver

--- install-algeb-solver.sh
+++ install-algeb-solver.sh
@@ -1,5 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
+pkg upgrade
+
+pkg install -y python wget
+
+pip install sympy
+
 cd ~
 
 wget https://github.com/cityofsmiles/calculators/raw/master/algeb-solver.zip
