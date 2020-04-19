1a4ba14dd75df937165943ee283fc5400f229451
jon <cityofsmiles@gmail.com>
2020-04-19 08:30:16 +0800

    edited gen-cal.py

--- gen_cal.py
+++ gen_cal.py
@@ -38,15 +38,18 @@ Please type in the calculator you want to use:
 ''') or "1") 
 
 	if calc_type == "1":
-		os.chdir(cur_dir/"algeb-solver-v2")
+		dir = 'algeb-solver-v2'
+		os.chdir(cur_dir/dir)
 		exec(open('algeb-solver-v2.py').read())
 		os.chdir(cur_dir)
 	elif calc_type == "2": 
-		os.chdir(cur_dir/"synthetic-calc") 
+		dir = 'synthetic-calc'
+		os.chdir(cur_dir/dir)
 		exec(open('synthetic-calc.py').read())
 		os.chdir(cur_dir)
 	elif calc_type == "3": 
-		os.chdir(cur_dir/"systems-solver") 
+		dir = 'systems-solver'
+		os.chdir(cur_dir/dir)
 		exec(open('systems-solver.py').read())
 		os.chdir(cur_dir)
 	else:
